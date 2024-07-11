04216d247e573c000962724063d2d0352a22170d
Marcela Moreti <108123117+mahlvm@users.noreply.github.com>
2024-07-11 17:34:58 +0100

    Update

--- MakersBnB/Models/MakersBnBDbContext.cs
+++ MakersBnB/Models/MakersBnBDbContext.cs
@@ -3,32 +3,32 @@ using Microsoft.EntityFrameworkCore;
 
 public class MakersBnBDbContext : DbContext
 {
-    // Representa a tabela Spaces, User e Reservation no banco de dados
+    
     internal DbSet<Space>? Spaces { get; set; }
     internal DbSet<User>? Users { get; set; }
     public DbSet<Reservation>? Reservations { get; set; }
 
-    // Caminho para o banco de dados
+    
     internal string? DbPath { get; }
 
-     // Nome do banco de dados
+
     internal string dbName = "makersbnb_aspdotnet_dev";
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Configura o relacionamento um-para-muitos entre User e Space
+            
             modelBuilder.Entity<User>()
                 .HasMany(u => u.Spaces)
                 .WithOne(s => s.User)
                 .HasForeignKey(s => s.UserId);
 
-            // Configura o relacionamento um-para-muitos entre Space e Reservation
+          
             modelBuilder.Entity<Space>()
                 .HasMany(s => s.Reservations)
                 .WithOne(r => r.Space)
                 .HasForeignKey(r => r.SpaceId);
 
-            // Configura o relacionamento um-para-muitos entre User e Reservation
+          
             modelBuilder.Entity<User>()
                 .HasMany(u => u.Reservations)
                 .WithOne(r => r.User)
@@ -37,7 +37,7 @@ public class MakersBnBDbContext : DbContext
 
     
     
-    // Configuração do banco de dados
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseNpgsql(
         @"Host=localhost;Username=postgres;Password=1234;Database=" + this.dbName
