4356b7948abe527e3408d58cd56777bd7c87a34a
Marcela Moreti <108123117+mahlvm@users.noreply.github.com>
2024-07-11 17:32:16 +0100

    Update SessionsController.cs

--- MakersBnB/Controllers/SessionsController.cs
+++ MakersBnB/Controllers/SessionsController.cs
@@ -44,10 +44,10 @@ namespace MakersBnB.Controllers
         [Route("/Sessions/Logout")]
         public IActionResult Logout()
         {
-            // Remove o usuário da sessão
+            
             HttpContext.Session.Remove("user_id");
 
-            // Redireciona para a página inicial
+
             return RedirectToAction("Index", "Home");
         }
 
