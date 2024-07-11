841c55ffaaf597566ed200a864e9138de71073f4
mahlvm <mahmoreti@hotmail.com>
2024-07-11 15:49:27 +0100

    done

--- MakersBnB/obj/Debug/net8.0/MakersBnB.AssemblyInfo.cs
+++ MakersBnB/obj/Debug/net8.0/MakersBnB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MakersBnB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd99df1ff6f083bdfe8e592d78afbd27a78db11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c008b94c67a9f097a83d3cf645286a964a2d22b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MakersBnB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MakersBnB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
