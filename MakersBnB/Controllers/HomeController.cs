47e899d57c649f76e07ad2a4a28581c13aa3f368
Marcela Moreti <108123117+mahlvm@users.noreply.github.com>
2024-07-11 17:30:48 +0100

    Update HomeController.cs

--- MakersBnB/Controllers/HomeController.cs
+++ MakersBnB/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace MakersBnB.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        // Certifique-se de que há apenas um construtor com parâmetros que o DI pode resolver
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -16,15 +16,15 @@ namespace MakersBnB.Controllers
 
         public IActionResult Index()
         {
-            // Verifica se o usuário está autenticado
+            
             if (HttpContext.Session.GetInt32("user_id") != null)
             {
-                // Se autenticado, redireciona para a página de listagem de espaços
+                
                 return Redirect("/Spaces");
             }
             else
             {
-                // Se não autenticado, redireciona para a página de login
+                
                 return Redirect("/Sessions/New");
             }
         }
@@ -32,10 +32,10 @@ namespace MakersBnB.Controllers
         [Route("/Home/Logout")]
         public IActionResult Logout()
         {
-            // Limpa a sessão
+            
             HttpContext.Session.Clear();
 
-            // Redireciona para a página inicial
+            
             return RedirectToAction("Index", "Home");
         }
 
